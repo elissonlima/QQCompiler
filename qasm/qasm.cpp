924e0545809d13c6f7b194effbe4272d916c7307
Elisson <31016448+elissonlima@users.noreply.github.com>
2025-09-27 21:20:18 -0300

    format update

--- qasm/qasm.cpp
+++ qasm/qasm.cpp
@@ -13,9 +13,9 @@ std::string QAsm::generate_qasm() const {
     for (const ZYZParams zyz_param : zyz_gates) {
 
         oss << "p(" << zyz_param.alpha << ") q;" << std::endl;
-        oss << "rz( " << zyz_param.beta << ") q;" << std::endl;
-        oss << "ry( " << zyz_param.gamma << ") q;" << std::endl;
-        oss << "rz( " << zyz_param.delta << ") q;" << std::endl;
+        oss << "rz(" << zyz_param.beta << ") q;" << std::endl;
+        oss << "ry(" << zyz_param.gamma << ") q;" << std::endl;
+        oss << "rz(" << zyz_param.delta << ") q;" << std::endl;
 
     }
 

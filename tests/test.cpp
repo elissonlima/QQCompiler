9c3f7bed96c9b7f527e2a04a64d3519cb2d032df
Alex Carvalho <jac@cin.ufpe.br>
2025-10-01 20:40:14 -0300

    refactor:  removing unnecessary commentaries

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,23 +1,19 @@
 #include <armadillo>
-#include <gtest/gtest.h> // Essencial para testes
+#include <gtest/gtest.h>
 
-#include "../qasm/qasm.h"       // Para ZYZParams
-#include "../unitary/unitary.h" // Para get_zyz_params_from_matrix
+#include "../qasm/qasm.h"       
+#include "../unitary/unitary.h" 
 
-// Os testes são definidos no escopo global, fora da main.
 TEST(UnitaryDecomposition, TestSQRTX) {
-    // Arrange
+    
     arma::cx_mat SQRT_X(2, 2, arma::fill::zeros);
     SQRT_X(0, 0) = arma::cx_double(0.5, 0.5);
     SQRT_X(0, 1) = arma::cx_double(0.5, -0.5);
     SQRT_X(1, 0) = arma::cx_double(0.5, -0.5);
     SQRT_X(1, 1) = arma::cx_double(0.5, 0.5);
 
-    // Act
     ZYZParams params = get_zyz_params_from_matrix(SQRT_X);
 
-    // Assert
-    // Use ASSERT_DOUBLE_EQ para comparar números de ponto flutuante!
     ASSERT_DOUBLE_EQ(params.alpha, arma::datum::pi / 4.0);
     ASSERT_DOUBLE_EQ(params.beta,  -arma::datum::pi / 2.0);
     ASSERT_DOUBLE_EQ(params.gamma,  arma::datum::pi / 2.0);
